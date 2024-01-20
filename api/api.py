ca84fc054dc10f610a1967e0d23ab4ce3e9fb160
Alexander Colloredo-Mansfeld <acollore@ucsc.edu>
2024-01-19 23:11:35 -0800

    working communication between react and flask

--- api/api.py
+++ api/api.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 import time
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
 
 @app.route('/time')
--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var LOCALHOST = 'http://127.0.0.1:5001'
+
 function App() {
+  const [currentTime, setCurrentTime] = useState(2)
+  console.log("hello")
+
+  useEffect(() => {
+    console.log("WE ARE SETTING TIME")
+    fetch(LOCALHOST + '/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +30,9 @@ function App() {
         >
           Learn React
         </a>
+        <p>
+          Current Time: {currentTime}
+        </p>
       </header>
     </div>
   );
